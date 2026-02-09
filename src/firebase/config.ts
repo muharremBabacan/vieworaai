a54be7f967547143342979e473c3afe3a6a4b3b0
Muharrem Babacan <babacan.muharrem@gmail.com>
2026-02-09 20:47:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8632782825-fce99",
+  "appId": "1:1093513393552:web:73dcc66ac9684e5237ef15",
+  "apiKey": "AIzaSyCIyLeBksCYIYDkUdq522hlMnvSKBq3VZw",
+  "authDomain": "studio-8632782825-fce99.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1093513393552"
+};
