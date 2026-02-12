2674c175ae7472ee4e31321f4c854d48e1bd000d
Muharrem Babacan <babacan.muharrem@gmail.com>
2026-02-12 16:16:01 +0000

    vertex rolerini ayarladım

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
   "projectId": "viewora-photo",
   "appId": "1:1093513393552:web:65a6f20e4395a1215b2447",
+  "storageBucket": "viewora-photo.appspot.com",
   "apiKey": "AIzaSyArCkWAaym7ybDnhCvNMJZMhs8IULCxhpc",
   "authDomain": "viewora-photo.firebaseapp.com",
-  "measurementId": "G-5G3XPEX156",
-  "storageBucket": "viewora-photo.appspot.com",
-  "messagingSenderId": "1093513393552"
+  "messagingSenderId": "1093513393552",
+  "measurementId": "G-5G3XPEX156"
 };
