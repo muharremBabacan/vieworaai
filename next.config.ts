554933495a47d335272186cc3eea3a62b0f51999
Muharrem Babacan <babacan.muharrem@gmail.com>
2026-02-12 13:08:09 +0000

    noldu analiz takıldı kaldı dönüüp duruyor

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   serverActions: {
     bodySizeLimit: '10mb',
+    executionTimeout: 180,
   },
   typescript: {
     ignoreBuildErrors: true,
