dd2cb7a1026bef119a3f439a7733d9de2c48fe78
Muharrem Babacan <babacan.muharrem@gmail.com>
2026-02-13 09:12:05 +0000

    noldu

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  serverActions: {
-    bodySizeLimit: '10mb',
-    executionTimeout: 180,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
