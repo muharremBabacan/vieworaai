98dd30dd32f208a59ebafa40f2f5e345a15591d7
Muharrem Babacan <babacan.muharrem@gmail.com>
2026-02-15 20:49:43 +0000

    viewora.ai den girmeli unutma

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,6 @@
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "studio-8632782825-fce99.firebaseapp.com",
+  authDomain: "viewora.ai",
   databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
