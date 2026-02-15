a582c3a902abedb6de6fdf278776709ae36b4a43
Muharrem Babacan <babacan.muharrem@gmail.com>
2026-02-15 15:24:01 +0000

    Evet yaptım çalışıyor ama Google girişi firebase üzerinden ve kullanıcıy

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,6 @@
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  authDomain: "viewora.ai",
   databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
