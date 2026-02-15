fff88338b957ff04fbd3ba50d3ec718855b094a4
Muharrem Babacan <babacan.muharrem@gmail.com>
2026-02-15 11:31:30 +0000

    studio-8632782825-fce99.firebaseapp.com uygulamasına devam edin googl gi

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyCIyLeBksCYIYDkUdq522hlMnvSKBq3VZw",
-  authDomain: "studio-8632782825-fce99.firebaseapp.com",
-  databaseURL: "studio-8632782825-fce99-default-rtdb.firebaseio.com",
-  projectId: "studio-8632782825-fce99",
-  storageBucket: "studio-8632782825-fce99.firebasestorage.app",
-  messagingSenderId: "1093513393552",
-  appId: "1:1093513393552:web:73dcc66ac9684e5237ef15"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
