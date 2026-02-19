94bf60d23a5adfc2b7a24680bf580d587900c49c
Muharrem Babacan <babacan.muharrem@gmail.com>
2026-02-19 04:32:17 +0000

    use-collection.tsx Bu dosyayı ver bana

--- src/app/(app)/gallery/page.tsx
+++ src/app/(app)/gallery/page.tsx
@@ -1,4 +1,3 @@
-'use client';
 // This file is intentionally left blank.
 // The content has been moved to src/app/[locale]/(app)/gallery/page.tsx
 // to support internationalized routing and prevent route conflicts.
