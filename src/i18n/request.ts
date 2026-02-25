9fc6e858ecde4b3bdc28b060eac7ad677180d7b9
Muharrem Babacan <babacan.muharrem@gmail.com>
2026-02-25 11:23:56 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '..

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -4,6 +4,6 @@ export default getRequestConfig(async ({locale}) => {
   // Validate that the incoming `locale` parameter is valid
   // For now, we'll always serve Turkish to stabilize the app.
   return {
-    messages: (await import(`../messages/tr.json`)).default
+    messages: (await import(`../../messages/tr.json`)).default
   };
 });
