9a8f91faa6b11919e5ecc871f392986fdfe405e5
Muharrem Babacan <babacan.muharrem@gmail.com>
2026-03-02 22:19:46 +0000

    fix explicit google auth for vertex

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,10 +1,19 @@
 import 'server-only';
 import { genkit } from 'genkit';
 import { vertexAI } from '@genkit-ai/vertexai';
+import { GoogleAuth } from 'google-auth-library';
+
+const auth = new GoogleAuth({
+  scopes: ['https://www.googleapis.com/auth/cloud-platform'],
+});
 
 export const ai = genkit({
   plugins: [
-    vertexAI({ location: 'us-central1' }),
+    vertexAI({
+      projectId: 'studio-8632782825-fce99',
+      location: 'us-central1',
+      googleAuth: auth,
+    }),
   ],
   model: 'vertexai/gemini-2.0-flash-001',
 });
